967e489104b5351e1723acfe5ea36b25f96800f3
Ikhsan <ikhsanfirdaus74@gmail.com>
2023-05-28 18:43:57 +0700

    seperate imports on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:mobile_flutter/views/auth/login_view.dart';
-import 'package:mobile_flutter/views/auth/auth_provider.dart';
-import 'package:mobile_flutter/views/auth/personal_form_view.dart';
-import 'package:mobile_flutter/views/auth/register_view.dart';
-import 'package:mobile_flutter/views/dashboard/dashboard_view.dart';
-import 'package:mobile_flutter/views/dashboard/product/detail_product_view.dart';
-import 'package:mobile_flutter/views/dashboard/product/product_provider.dart';
-import 'package:mobile_flutter/views/dashboard/user_setting/setting_address_view.dart';
-import 'package:mobile_flutter/views/dashboard/user_setting/setting_email_view.dart';
-import 'package:mobile_flutter/views/dashboard/user_setting/setting_name_view.dart';
-import 'package:mobile_flutter/views/dashboard/user_setting/setting_password_view.dart';
-import 'package:mobile_flutter/views/dashboard/user_setting/setting_phone_view.dart';
-import 'package:mobile_flutter/views/dashboard/user_setting/setting_view.dart';
-import 'package:mobile_flutter/views/dashboard/welcome_view.dart';
-import 'package:mobile_flutter/views/splash_screen_view.dart';
-import 'package:provider/provider.dart';
+part of imports;
 
 void main() async {
   await dotenv.load(fileName: ".env");
