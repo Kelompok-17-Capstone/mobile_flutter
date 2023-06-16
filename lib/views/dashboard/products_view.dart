62677f0b5ebad0e5ea90e0e2304800cc08440d6b
Ikhsan <ikhsanfirdaus74@gmail.com>
2023-06-16 09:22:41 +0700

    changing from 3 tabs to 1 tab & sort by price button

--- lib/views/dashboard/products_view.dart
+++ lib/views/dashboard/products_view.dart
@@ -4,6 +4,12 @@ import 'package:mobile_flutter/shared/products_grid.dart';
 import 'package:mobile_flutter/views/dashboard/product/product_provider.dart';
 import 'package:provider/provider.dart';
 
+enum PriceState {
+  none,
+  ascending,
+  descending
+}
+
 class ProductsView extends StatefulWidget {
   const ProductsView({super.key});
 
@@ -14,11 +20,12 @@ class ProductsView extends StatefulWidget {
 class _ProductsViewState extends State<ProductsView> with TickerProviderStateMixin{
 
   late TabController _tabController;
+  PriceState priceState = PriceState.none;
   
   @override
   void initState() {
     // Specifies number of Tabs here
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: 2, vsync: this);
     super.initState();
   }
 
@@ -40,75 +47,86 @@ class _ProductsViewState extends State<ProductsView> with TickerProviderStateMix
             Container(
               color: Colors.white,
               height: 40,
-              child: TabBar(
-                controller: _tabController,
-                labelColor: const Color(0xFF264ECA), //<-- selected text color
-                unselectedLabelColor: const Color(0xFF7D828C),
-                indicatorColor: const Color(0xFF264ECA),
-                tabs: [
-                  const Tab(
-                    child: Text(
-                      'Terbaru',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                      )
-                    ),
-                  ),
-                  const Tab(
-                    child: Text(
-                      'Terfavorit',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                      )
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        'Harga',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TabBar(
+                      controller: _tabController,
+                      labelColor: const Color(0xFF264ECA), //<-- selected text color
+                      unselectedLabelColor: Colors.grey,
+                      indicatorColor: const Color(0xFF264ECA),
+                      tabs: const [
+                        Tab(
+                          child: Text(
+                            'Terbaru',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
                         ),
+                        Tab(
+                          child: Text(
+                            'Terfavorit',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            )
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (priceState == PriceState.none) {
+                          priceState = PriceState.ascending;
+                        } else if (priceState == PriceState.ascending) {
+                          priceState = PriceState.descending;
+                        } else {
+                          priceState = PriceState.none;
+                        }
+                      });
+                      print(priceState);
+                    },
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.33,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Harga',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: priceState == PriceState.none ? Colors.grey : const Color(0xFF264ECA)
+                            ),
+                          ),
+                          Icon(
+                            priceState == PriceState.none
+                            ? Icons.unfold_more_outlined
+                            : priceState == PriceState.ascending
+                            ? Icons.expand_less_outlined
+                            : Icons.expand_more_outlined, // unfold_more_rounded
+                            size: 18,
+                            color: priceState == PriceState.none ? Colors.grey : const Color(0xFF264ECA),
+                          ),
+                        ]
                       ),
-                      Icon(
-                        Icons.unfold_more_rounded,
-                        size: 18,
-                      ),
-                    ]
+                    ),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: TabBarView(
-                physics: const NeverScrollableScrollPhysics(),
-                controller: _tabController,
-                children: [
-                  SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                      child: productsGrid(products: products, isProductPage: true)
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                      child: productsGrid(products: products, isProductPage: true),
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                      child: productsGrid(products: products, isProductPage: true),
-                    ),
-                  ),
-                ],
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: productsGrid(products: products, isProductPage: true),
+                ),
               ),
-            ),
+            )
           ],
         ),
       ),
